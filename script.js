a12d7797fcdc354a68f27c8d588702c1a9cd0274
simonegreco5 <simoxey.greco@gmail.com>
2026-03-18 20:26:58 +0100

    add condition in event listeners for calculate discounts price (under 18 / over 65)

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 console.log('js-biglietto')
 
 // creiamo le variabili per selezionare i nodi del dom 
-const KmRunScript = document.getElementById('km-run')
+const kmRunScript = document.getElementById('km-run')
 const userAgeScript = document.getElementById('user-age')
 const buttonScript = document.getElementById('button')
 const formScript = document.querySelector('form')
 
+// dato - costante prezzo per chilometro
+const kmPrice = 0.21
 
 // scriviamo la funzione usando il metodo event listeners
 // aggiungiamo alla funzione il parametro (e).preventDefault()
@@ -13,8 +15,27 @@ const formScript = document.querySelector('form')
 formScript.addEventListener('submit', function(e){
     e.preventDefault();
 
-    console.log(KmRunScript.value)
+    console.log(kmRunScript.value)
     console.log(userAgeScript.value)
+
+    // elaborazione dati
+    let userKmRun = kmRunScript.value
+    let userAge = userAgeScript.value
+
+    let fullPrice = kmPrice * userKmRun
+    console.log(fullPrice)
+
+    // condizione scontistiche (under 18 / over 65)
+    if (userAge < 18){
+        console.log(fullPrice * (1 - 20 / 100))
+
+    } else if (userAge > 65){
+        console.log(fullPrice * (1 - 40 / 100))
+
+    } else {
+        console.log(fullPrice)
+
+    }
 
 })
 
