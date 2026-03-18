738ce60a051a89f66366af42ae15046e2408cfce
simonegreco5 <simoxey.greco@gmail.com>
2026-03-19 00:27:10 +0100

    add fix and (MILESTONE 3)

--- script.js
+++ script.js
@@ -27,38 +27,48 @@ formScript.addEventListener('submit', function(e){
     console.log(fullPrice)
 
 
-    // creiamo le variabili dove poi aggiungeremo i vari prezzi
-    let priceUnder = ''
-    let priceOver = ''
+    // creiamo la variabile dove poi aggiungeremo il prezzo selezionato nella condizionale
+    let finalPrice
+    
 
     // condizione scontistiche (under 18 / over 65)
     if (userAge < 18){
         // console.log(fullPrice * (1 - 20 / 100))
-        priceUnder = fullPrice * (1 - 20 / 100)
-    
+        finalPrice = fullPrice * (1 - 20 / 100)
 
     } else if (userAge > 65){
         // console.log(fullPrice * (1 - 40 / 100))
-        priceOver = fullPrice * (1 - 40 / 100)
-
+        finalPrice = fullPrice * (1 - 40 / 100)
         
     } else {
-        fullPrice 
+        finalPrice = fullPrice 
     
     }
 
-    // stampiamo i prezzi con massimo 2 decimali (creando nuove variabili)
+    // stampiamo i prezzi con massimo 2 decimali (creando una nuova variabile)
     // usando il metodo .toFixed() 
     // --> questo ci servirà per aggiungerli successivamente nella card finale che l'utente vedrà
     // adoperiamo Number() per rendere i numeri dei valori numerici e non più delle stringhe
 
     // prezzo under 18 (-20%)
-    let resultUnder = Number(priceUnder).toFixed(2)
-    // prezzo over 65 (-40%)
-    let resultOver = Number(priceOver).toFixed(2)
-    // prezzo full
-    let resultFull = fullPrice.toFixed(2)
+    let finalResult = Number(finalPrice).toFixed(2)
+    
 
+    // selezioniamo i nodi del dom della card biglietto 
+    const numero = document.querySelector('.numero')
+    const prezzo = document.querySelector('.prezzo')
+    const posto = document.querySelector('.posto')
+    const carrozza = document.querySelector('.carrozza')
+
+    // generiamo dei numeri casuali per il biglietto
+    let postoCasuale = Math.floor((Math.random() * 30) + 1)
+    let carrozzaCasuale = Math.floor((Math.random() * 12) + 1)
+    let numeroCasuale = Math.floor((Math.random() * 10000) + 1000)
+
+    numero.innerText = numeroCasuale
+    prezzo.innerText = `${finalResult} €`
+    posto.innerText = `${postoCasuale}B`
+    carrozza.innerText = carrozzaCasuale
     
 
 })
